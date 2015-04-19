b6d407edaf2d583fcbba679c449ddbbeecb43f4f
Alexandre <bighal.cormier@gmail.com>
2015-04-19 11:56:24 +0200

    Update score.php

--- score.php
+++ score.php
@@ -1,8 +1,6 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "games";
+
+include ('db.php');
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
